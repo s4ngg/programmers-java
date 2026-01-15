3ede40695786e714a95ab21caa1c74cd77855922
Sang woo Kim <50235048+s4ngg@users.noreply.github.com>
2026-01-15 15:21:34 +0900

    Create 머쓱이보다 키 큰 사람.java

---
+++ Level0/머쓱이보다 키 큰 사람.java
@@ -0,0 +1,13 @@
+import java.util.Arrays;
+
+class Solution {
+    public int solution(int[] array, int height) {
+        int answer = 0;
+        Arrays.sort(array);
+        
+        for(int i : array){
+            if(i > height) { answer ++; }
+        }
+        return answer;
+    }
+}
