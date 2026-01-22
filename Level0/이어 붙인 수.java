6fc7411c252ed1fe2b5b76e73e755403dcb730aa
Sang woo Kim <50235048+s4ngg@users.noreply.github.com>
2026-01-22 12:19:32 +0900

    Create 이어 붙인 수.java

---
+++ Level0/이어 붙인 수.java
@@ -0,0 +1,16 @@
+class Solution {
+    public int solution(int[] num_list) {
+        String odd = "";
+        String even = "";
+        
+        for (int i=0; i<num_list.length; i++){
+            if (num_list[i] % 2 == 0) {
+                even += String.valueOf(num_list[i]);
+            } else {
+                odd += String.valueOf(num_list[i]);
+            }
+        }
+        int answer = Integer.parseInt(odd) + Integer.parseInt(even); 
+        return answer;
+    }
+}
