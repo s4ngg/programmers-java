8c808c2829588510624609a059312c2bac034bc2
Sang woo Kim <50235048+s4ngg@users.noreply.github.com>
2026-01-29 18:06:51 +0900

    Create 부족한 금액 계산하기.java

---
+++ Level1/부족한 금액 계산하기.java
@@ -0,0 +1,11 @@
+class Solution {
+    public long solution(int price, int money, int count) {
+        long answer = -1;
+        long sum = 0;
+        for (int i=0; i<count; i++) {
+            sum += price*(i+1);
+        }
+        answer = sum - money;
+        return answer > 0 ? answer : 0;
+    }
+}
